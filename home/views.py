ace13d3386845dccfaf0d03d19b9f0c1fc9e43f2
Rithk <rithkchaudhary86@gmail.com>
2023-07-30 00:07:49 +0530

    the registration page is completed

--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
    return render(request,'home/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            
+            return redirect('/')
+    else:
+        form = UserRegisterForm()
+        return render(request,'home/register.html',{'form': form})
+    
+    
